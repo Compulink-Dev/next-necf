39019a61e226053cb2b9361299c751111a21a6ef
Compulink-Dev <devcompulink@gmail.com>
2024-05-14 09:47:05 +0200

    main

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,61 +1,70 @@
-'use client'
+// 'use client'
+// import React from 'react'
+// import DashboardCard from './_components/DashboardCard'
+// import TotalForm from './_components/TotalForm'
+// import AnnouncementForm from './_components/AnnouncementForm'
+// import ActivityForm from './_components/ActivityForm'
+// import ScheduleForm from './_components/ScheduleForm'
+// import { checkAuth } from '@/middleware/auth'
+// import { NextPageContext } from 'next'
+// import { useRouter } from 'next/navigation'
+
+// interface DashboardProps {
+//   isAuthorized: boolean
+// }
+
+// export default function Dashboard({ isAuthorized }: DashboardProps) {
+
+//   const router = useRouter()
+
+//   // if (!isAuthorized) {
+//   //     return router.push('/login');
+//   // }
+
+//   return (
+//     <div className='mt-4'>
+//       <div className="flex gap-2">
+//         <div className="" style={{ flex: 4 }}>
+//           <div className="flex gap-4">
+//             <DashboardCard />
+//             <DashboardCard />
+//             <DashboardCard />
+//           </div>
+//           <div className="mt-4 flex gap-4">
+//             <TotalForm />
+//             <TotalForm />
+//           </div>
+//           <div className="my-4">
+//             <AnnouncementForm />
+//           </div>
+//         </div>
+
+//         <div className="" style={{ flex: 2 }}>
+//           <ActivityForm />
+//           <div className="my-4">
+//             <ScheduleForm />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// Dashboard.getInitialProps = async (
+//   context: NextPageContext
+// ): Promise<DashboardProps> => {
+//   //@ts-ignore
+//   const isAuthorized = await checkAuth(context.req, context.res);
+//   return { isAuthorized };
+// };
+
+
 import React from 'react'
-import DashboardCard from './_components/DashboardCard'
-import TotalForm from './_components/TotalForm'
-import AnnouncementForm from './_components/AnnouncementForm'
-import ActivityForm from './_components/ActivityForm'
-import ScheduleForm from './_components/ScheduleForm'
-import { checkAuth } from '@/middleware/auth'
-import { NextPageContext } from 'next'
-import { useRouter } from 'next/navigation'
 
-interface DashboardProps {
-  isAuthorized: boolean
-}
-
-function Dashboard({ isAuthorized }: DashboardProps) {
-
-  const router = useRouter()
-
-  // if (!isAuthorized) {
-  //     return router.push('/login');
-  // }
-
+function Dashboard() {
   return (
-    <div className='mt-4'>
-      <div className="flex gap-2">
-        <div className="" style={{ flex: 4 }}>
-          <div className="flex gap-4">
-            <DashboardCard />
-            <DashboardCard />
-            <DashboardCard />
-          </div>
-          <div className="mt-4 flex gap-4">
-            <TotalForm />
-            <TotalForm />
-          </div>
-          <div className="my-4">
-            <AnnouncementForm />
-          </div>
-        </div>
-
-        <div className="" style={{ flex: 2 }}>
-          <ActivityForm />
-          <div className="my-4">
-            <ScheduleForm />
-          </div>
-        </div>
-      </div>
-    </div>
+    <div>Dashboard</div>
   )
 }
-
-Dashboard.getInitialProps = async (
-  context: NextPageContext
-): Promise<DashboardProps> => {
-  //@ts-ignore
-  const isAuthorized = await checkAuth(context.req, context.res);
-  return { isAuthorized };
-};
 
 export default Dashboard
