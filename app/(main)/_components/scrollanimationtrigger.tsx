c65554e7305ec6354782d8627c1a38362a6f9c5f
Compulink-Dev <devcompulink@gmail.com>
2024-05-22 16:33:25 +0200

    new changes except the programs

---
+++ app/(main)/_components/scrollanimationtrigger.tsx
@@ -0,0 +1,26 @@
+// "use client";
+// import React from "react";
+// import { motion } from "framer-motion";
+// function ScrollAnimationTrigger() {
+//   return (
+//     <div className="flex flex-col items-center">
+//       <h1>Scroll animation</h1>
+//       <Box /> <Box /> <Box />
+//     </div>
+//   );
+// }
+
+// function Box() {
+//   return (
+//     <motion.div
+//       className="my-[200px] mx-0 w-20 h-20 bg-blue-950"
+//       initial={{ opacity: 0, scale: 0, x: -100 }}
+//       whileInView={{ opacity: 1, scale: 1, x: 0 }}
+//       transition={{ duration: 0.6 }}
+//     >
+//       <h1>Tinashe</h1>
+//     </motion.div>
+//   );
+// }
+
+// export default ScrollAnimationTrigger;
