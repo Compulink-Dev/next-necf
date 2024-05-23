798ecfdde8f3ada9a5376f1d5b5e009993cfb552
Compulink-Dev <devcompulink@gmail.com>
2024-05-23 11:38:15 +0200

    final updates

---
+++ constants/hocprograms.ts
@@ -0,0 +1,8 @@
+export const hocprograms = [
+  {
+    id: 1,
+    title: "Hoc Programs",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+    imageUrl: "/loading.gif",
+  },
+];
