1cea86e820e5bbcf155842a897a97e51b1c8863b
Compulink-Dev <devcompulink@gmail.com>
2024-05-23 11:38:15 +0200

    final updates

--- app/(main)/programs/_components/galleryModal.tsx
+++ app/(main)/programs/_components/galleryModal.tsx
@@ -1,52 +1,56 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import GalleryCard from "./galleryCard"
-import Image from "next/image"
-
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import GalleryCard from "./galleryCard";
+import Image from "next/image";
 
 interface GalleryProps {
-    id: number,
-    imageUrl: string,
-    title: string,
-    desc: string
+  id: number;
+  imageUrl: string;
+  title: string;
+  desc: string;
 }
 
-
 export function GalleryModal({ id, imageUrl, title, desc }: GalleryProps) {
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <GalleryCard
-                    key={id}
-                    imageUrl={imageUrl}
-                    title={title}
-                    description={desc}
-                />
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
-                    <DialogDescription>
-                        Make changes to your profile here. Click save when you are done.
-                    </DialogDescription>
-                </DialogHeader>
-                <div className="w-full h-full">
-                    <Image src={imageUrl} alt="" width={500} height={500} className="w-full h-full" />
-                </div>
-                <DialogFooter>
-                    <Button type="submit">Save changes</Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    )
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <GalleryCard
+          key={id}
+          imageUrl={imageUrl}
+          title={title}
+          description={desc}
+        />
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you are done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="w-full h-full">
+          <Image
+            src={imageUrl}
+            alt=""
+            width={500}
+            height={500}
+            className="w-full h-full"
+          />
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
---
+++ constants/taskforces.ts
@@ -0,0 +1,8 @@
+export const taskforces = [
+  {
+    id: 1,
+    title: "Task Force One",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. ",
+    imageUrl: "/logo.png",
+  },
+];
