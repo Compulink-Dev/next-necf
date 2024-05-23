5da5099425e758b1a6d87ea59178ab92567de4eb
Compulink-Dev <devcompulink@gmail.com>
2024-05-23 14:54:38 +0200

    new updates at 14:54

--- app/(main)/team/_components/TeamCard.tsx
+++ app/(main)/team/_components/TeamCard.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import { MdPerson } from "react-icons/md";
 import { FaFacebook, FaLinkedin } from "react-icons/fa";
@@ -7,41 +7,50 @@ import Link from "next/link";
 import Image from "next/image";
 
 interface TeamCardProps {
-  name: string,
-  imageUrl?: string,
-  title: string,
-  desc?: string,
-  fbLink?: string,
-  twitterLink?: string,
-  gmail?: string
+  name: string;
+  imageUrl?: string;
+  title: string;
+  desc?: string;
+  fbLink?: string;
+  twitterLink?: string;
+  gmail?: string;
 }
 
-
-
-function TeamCard({ name, imageUrl, title, desc, fbLink, twitterLink, gmail }: TeamCardProps) {
+function TeamCard({
+  name,
+  imageUrl,
+  title,
+  desc,
+  fbLink,
+  twitterLink,
+  gmail,
+}: TeamCardProps) {
   return (
     <div className="w-72 h-96 border border-green-800 flex flex-col rounded">
       <div className="flex-1">
-        {
-          imageUrl && imageUrl.trim().length > 0 ? (
-            <MdPerson size={250} color="green" className="mx-auto" />
-          )
-            : (
-              <Image src={`/${imageUrl}`} alt="logo" width={200} height={200} className="w-full h-full rounded-full" />
-            )
-        }
+        {imageUrl && imageUrl.trim().length > 0 ? (
+          <Image
+            src={imageUrl}
+            alt="logo"
+            width={100}
+            height={100}
+            className="w-60 h-60 rounded-full mx-auto object-cover"
+          />
+        ) : (
+          <MdPerson size={250} color="green" className="mx-auto" />
+        )}
       </div>
       <div className="flex-1 flex flex-col items-center justify-center gap-1 w-full h-[35%] bg-green-800 object-cover">
         <p className="text-white font-semibold text-lg ">{name}</p>
         <p className="text-gray-300 text-sm">{title}</p>
         <div className="flex items-center justify-center gap-12 mx-auto">
-          <Link href={'/'}>
+          <Link href={"/"}>
             <FaFacebook size={18} color="white" />
           </Link>
-          <Link href={'/'}>
+          <Link href={"/"}>
             <BsTwitterX size={18} color="white" />
           </Link>
-          <Link href={'/'}>
+          <Link href={"/"}>
             <FaLinkedin size={18} color="white" />
           </Link>
         </div>
