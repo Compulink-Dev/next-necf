51777a8811e62edc8669620f5fc5ea9c99ba779f
Compulink-Dev <devcompulink@gmail.com>
2024-05-27 09:32:33 +0200

    modified:   app/(main)/_components/mainhero.tsx

--- app/(main)/_components/mainhero.tsx
+++ app/(main)/_components/mainhero.tsx
@@ -45,6 +45,19 @@ function MainHero() {
             </div>
             <TopSection />
           </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full h-[50%]">
+              <div
+                className="bg-scroll bg-cover bg-center
+   bg-no-repeat bg-[url('/NECF.jpg')] w-full h-[40%] object-cover "
+              >
+                <div className="flex flex-col items-center justify-center bg-scroll bg-center bg-black w-full h-screen bg-opacity-25">
+                  <div className=" flex gap-3 text-center text-xl md:text-5xl  text-gray-300 py-4"></div>
+                </div>
+              </div>
+            </div>
+            <TopSection />
+          </SwiperSlide>
         </Swiper>
       </div>
     </>
