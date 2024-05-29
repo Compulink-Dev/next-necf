a777ade6e36c97edc2a23863a2623cecbe7d1aed
Compulink-Dev <devcompulink@gmail.com>
2024-05-29 12:56:21 +0200

    main

--- app/(dashboard)/dashboard/team/_components/TeamTab.tsx
+++ app/(dashboard)/dashboard/team/_components/TeamTab.tsx
@@ -1,3 +1,4 @@
+import CreateButton from "@/components/createButton"
 import { DatePicker } from "@/components/dateTimePicker"
 import { Button } from "@/components/ui/button"
 import {
@@ -18,8 +19,13 @@ import {
 } from "@/components/ui/tabs"
 import { Textarea } from "@/components/ui/textarea"
 import Upload from "@/components/upload"
+import ChairsList from "./ChairsList"
+import getChairs from "@/lib/team/getChairs"
 
-export function TeamTab() {
+export async function TeamTab() {
+
+    const chairs = await getChairs()
+
     return (
         <Tabs defaultValue="account" className="w-full">
             <TabsList className="flex gap-2 bg-green-600 text-white">
@@ -58,19 +64,10 @@ export function TeamTab() {
                         </CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-2">
-                        <Upload />
-                        <div className="space-y-1">
-                            <Label htmlFor="title">Title</Label>
-                            <Input id="title" />
+                        <div className="pb-4 flex justify-end">
+                            <CreateButton />
                         </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="position">Position</Label>
-                            <Input id="position" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="link1">Facebook Link</Label>
-                            <Input id="link1" />
-                        </div>
+                        <ChairsList chairs={chairs} />
                     </CardContent>
                     <CardFooter>
                         <Button className="bg-green-600 hover:bg-green-500">Save password</Button>
