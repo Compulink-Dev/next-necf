ddc42c911cd98f29309e96013846faa0fee37a69
Compulink-Dev <devcompulink@gmail.com>
2024-05-31 15:46:41 +0200

    changed footer link

--- app/(dashboard)/dashboard/vacancy/page.tsx
+++ app/(dashboard)/dashboard/vacancy/page.tsx
@@ -1,40 +1,36 @@
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import React from 'react'
-import VacancyForm from './_components/vacancyForm'
-import AddVacancy from './_components/formVacancy'
-import BackButton from '@/components/backButton'
-import VacancyList from './_components/vacancyList'
-import getVacancies from '@/lib/vacancy/getVacancies'
-import CreateButton from '@/components/createButton'
-import VacanciesCard from './_components/vacanciesCard'
-
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React, { useEffect } from "react";
+import VacancyForm from "./_components/vacancyForm";
+import AddVacancy from "./_components/formVacancy";
+import BackButton from "@/components/backButton";
+import VacancyList from "./_components/vacancyList";
+import getVacancies from "@/lib/vacancy/getVacancies";
+import CreateButton from "@/components/createButton";
+import VacanciesCard from "./_components/vacanciesCard";
 
 async function Vacancy() {
+  const vacancies = await getVacancies();
+  console.log(vacancies);
 
-    const vacancies = await getVacancies()
-    console.log(vacancies);
-
-    return (
-        <div className='p-4'>
-            <div className="pb-4 flex justify-end">
-                <CreateButton />
-            </div>
-            <div>
-                {
-                    vacancies.map((vacancy: any) => (
-                        <VacanciesCard
-                            key={vacancy.id}
-                            link={vacancy.id}
-                            name={vacancy.name}
-                            jobType={vacancy.jobType}
-                            dueDate={vacancy.dueDate}
-                        />
-                    ))
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="p-4">
+      <div className="pb-4 flex justify-end">
+        <CreateButton />
+      </div>
+      <div>
+        {vacancies?.map((vacancy: any) => (
+          <VacanciesCard
+            key={vacancy._id}
+            link={vacancy._id}
+            name={vacancy.name}
+            jobType={vacancy.jobType}
+            dueDate={vacancy.dueDate}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Vacancy
+export default Vacancy;
