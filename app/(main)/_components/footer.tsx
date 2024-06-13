f03660fa99b54f7b41cbc1156ec06420a5a2ea20
Compulink-Dev <devcompulink@gmail.com>
2024-06-13 08:38:31 +0200

    main

--- app/(main)/_components/footer.tsx
+++ app/(main)/_components/footer.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { FaFacebook, FaLinkedin } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
 import Link from "next/link";
+import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
@@ -118,7 +119,7 @@ function Footer() {
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
             <Link
               className="text-white hover:text-slate-600"
-              href={"/"}
+              href={"https://www.facebook.com/necfpage"}
               target="_blank"
             >
               <svg
@@ -133,20 +134,11 @@ function Footer() {
               </svg>
             </Link>
             <Link
-              href={"/"}
+              href={"https://x.com/FofficialNec?mx=2"}
               target="_blank"
-              className="ml-3 text-white hover:text-slate-600"
+              className="ml-3 pt-1 text-white hover:text-slate-600"
             >
-              <svg
-                fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
+              <FaXTwitter />
             </Link>
             <Link
               href={"/"}
