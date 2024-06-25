1f21c275be2abe699b8029c48574d2d51128a785
Compulink-Dev <devcompulink@gmail.com>
2024-06-25 15:14:23 +0200

    main

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,18 @@
 const nextConfig = {
     images: {
         domains: ['res.cloudinary.com']
-    }
+    },
+    headers: () => [
+        {
+            source: '/:/*',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store',
+                },
+            ],
+        },
+    ],
 };
 
 export default nextConfig;
