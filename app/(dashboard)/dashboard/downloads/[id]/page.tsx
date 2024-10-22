d9084493f9f784832e97abc2128ba10cca534569
Compulink-Dev <devcompulink@gmail.com>
2024-10-22 15:07:21 +0200

    main

--- app/(dashboard)/dashboard/downloads/[id]/page.tsx
+++ app/(dashboard)/dashboard/downloads/[id]/page.tsx
@@ -9,23 +9,6 @@ import DeleteButton from '../../_components/DeleteButton'
 //@ts-ignore
 async function DownloadEdit({ params: { id } }) {
     const download = await getDownload(id)
-
-    async function handleDelete() {
-        try {
-
-            const res = await fetch(`/api/downloads/${id}`, {
-                method: 'DELETE'
-            })
-            const data = await res.json()
-            console.log("Button id", id);
-            return data
-        } catch (error) {
-            console.log(error);
-
-        }
-    }
-
-
     return (
         <div className='p-4'>
             <div className="flex items-center justify-end gap-2">
